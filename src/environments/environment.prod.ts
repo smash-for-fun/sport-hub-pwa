e1bed30f4d721949af3623864ca3f28a98f9cb6f
Glenn Latomme <glenn.latomme@gmail.com>
2019-01-13 12:43:22 +0100

    Files shouldn't be hidden

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAI_Z7-qju3zkUaXK7yW68FKDrMMXBeAeM',
+    authDomain: 'smash-for-fun-sport-hub.firebaseapp.com',
+    databaseURL: 'https://smash-for-fun-sport-hub.firebaseio.com',
+    projectId: 'smash-for-fun-sport-hub',
+    storageBucket: 'smash-for-fun-sport-hub.appspot.com',
+    messagingSenderId: '467616779866'
+  },
+  hmr: false
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  api: 'http://localhost:5000/smash-for-fun-sport-hub/us-central1/api',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAI_Z7-qju3zkUaXK7yW68FKDrMMXBeAeM',
+    authDomain: 'smash-for-fun-sport-hub.firebaseapp.com',
+    databaseURL: 'https://smash-for-fun-sport-hub.firebaseio.com',
+    projectId: 'smash-for-fun-sport-hub',
+    storageBucket: 'smash-for-fun-sport-hub.appspot.com',
+    messagingSenderId: '467616779866'
+  },
+  hmr: false
+};
+
+/*
+ * In development mode, for easier debugging, you can ignore zone related error
+ * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
+ * below file. Don't forget to comment it out in production mode
+ * because it will have a performance impact when errors are thrown
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
